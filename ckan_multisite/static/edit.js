7636fa264b787752b31367b5d32f10ec4df10409
Jack McCracken <jackamc19@gmail.com>
2015-06-23 13:46:43 -0400

    Move back to using jsonify - which also simplifies our JavaScript

--- ckan_multisite/static/edit.js
+++ ckan_multisite/static/edit.js
@@ -3,15 +3,13 @@ $(function () {
     function do_api_request(endpoint, success, failure) {
         if (success == undefined) {
             success = function (data) {
-                obj = JSON.parse(data);
-                $("#alert_field").text(obj.success);
+                $("#alert_field").text(data.success);
                 $("#alert_field").removeClass('hidden');
             };
         }
         if (failure == undefined) {
             failure = function (data) {
-                obj = JSON.parse(data);
-                $("#alert_field").text("Error: " + obj.error);
+                $("#alert_field").text("Error: " + data.error);
                 $("#alert_field").removeClass('hidden');
             };
         }
@@ -26,17 +24,16 @@ $(function () {
     }
     
     $("#start_button").click(function () {
-        do_api_request("start")
+        do_api_request("start");
     });
 
     $("#stop_button").click(function () {
-        do_api_request("stop")
+        do_api_request("stop");
     });
 
     $("#status_button").click(function () {
         do_api_request("status", function (data) {
-            obj = JSON.parse(data);
-            $("#alert_field").text("Default port: " + obj.default_port + " Containers Running: " + obj.containers_running);
+            $("#alert_field").text("Default port: " + data.default_port + " Containers Running: " + data.containers_running);
             $("#alert_field").removeClass('hidden');
         })
     })
