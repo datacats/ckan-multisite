046a3e5333fe524a4f616b9120b0becf700455bc
Jack McCracken <jackamc19@gmail.com>
2015-07-20 20:04:24 -0400

    Kill celery process using $! variable

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-trap "sudo killall celery" SIGINT SIGTERM SIGKILL
-
 redis-server redis.conf
 celery -A ckan_multisite.task worker &
+trap "kill $!" EXIT
+
 if [ "$1" == "prod" ]; then
     uwsgi --ini uwsgi.ini
 else
